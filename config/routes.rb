e33e61dd6180574e8ea5e388a0b252f3ba62de09
givonconner1 <givonconner1@live.com>
2018-11-07 00:53:49 -0500

    stylesheet updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :availabilities
-   root to: "availabilities#index"
+   root to: "pages#index"
   
   get 'pages/dashboard' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
