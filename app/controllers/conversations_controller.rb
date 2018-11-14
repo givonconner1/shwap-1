581af4d6ad06b4babfa1ec7319aac9bfe0672182
chris.lam.unt <chris.lam.unt@gmail.com>
2018-11-14 17:59:22 -0500

    messaging is working

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,6 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
 
-#just added in Lams office 
-  layout false 
 
   def index
     @users = User.all
@@ -18,13 +16,7 @@ class ConversationsController < ApplicationController
     redirect_to conversation_messages_path(@conversation)
   end
 
-#just added right now in Lams office 
-  def show
-    @conversation = Conversation.find(params[:id])
-    @reciever = interlocutor(@conversation)
-    @messages = @conversation.messages
-    @message = Message.new
-  end
+
 
 private
   def conversation_params
