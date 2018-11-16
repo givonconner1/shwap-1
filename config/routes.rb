0cf0f07336cfcb6c50e4295a5db2e2ac7a6e3d1b
ChristophMartinez <christophermartinez9@my.unt.edu>
2018-11-16 01:21:02 -0600

    small readability changes in messages and conversations show files

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   resources :availabilities do
-    resources :requests 
+  resources :requests 
   end 
-   root to: "availabilities#index"
+  
+  root to: "availabilities#index"
   resources :conversations do
-    resources :messages
+  resources :messages
   end
   get 'pages/dashboard' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
