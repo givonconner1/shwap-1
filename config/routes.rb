260b60d3019e523c6d75de7b9cbb5662f84e1906
givonconner1 <givonconner1@live.com>
2018-12-12 13:54:10 -0500

    final shwap v1.0

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get "/pages/:page" => "pages#messages"
   get "/pages/:page" => "pages#new-staff"
   get "/pages/:page" => "pages#schedules"
+  get "/pages/:page" => "pages#about"
   
 
 end
